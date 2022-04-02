0013a945ed5a39bd18712244946c82e0beef8080
Angelica <Angelica137@users.noreply.github.com>
2022-04-02 20:38:22 +0100

    add create_inventory(), green

---
+++ scripts/inventory.py
@@ -0,0 +1,8 @@
+def create_inventory(items: list) -> dict:
+    inventory = dict()
+    for item in items:
+        if item in inventory:
+            inventory[item] += 1
+        else:
+            inventory[item] = 1
+    return inventory
