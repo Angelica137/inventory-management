1395782dd98d4d502529ffc2524ebd2d4561a372
Angelica <Angelica137@users.noreply.github.com>
2022-04-02 20:41:52 +0100

    test ad_items(), red

--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -4,3 +4,9 @@ from scripts.inventory import *
 def test_create_inventory():
     items = ["coal", "wood", "wood", "diamond", "diamond", "diamond"]
     assert create_inventory(items) == {"coal": 1, "wood": 2, "diamond": 3}
+
+
+def test_add_items():
+    inventory = {"coal": 1}
+    items = ["wood", "iron", "coal", "wood"]
+    assert add_items(inventory, items) == {"coal": 2, "wood": 2, "iron": 1}
