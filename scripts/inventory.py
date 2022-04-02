6ba85e4b1283401d0bad0a26a946529445fef942
Angelica <Angelica137@users.noreply.github.com>
2022-04-02 20:50:48 +0100

    delete obsolete

--- scripts/inventory.py
+++ scripts/inventory.py
@@ -1,12 +1,6 @@
 def create_inventory(items: list) -> dict:
     inventory = dict()
     return add_items(inventory, items)
-    for item in items:
-        if item in inventory:
-            inventory[item] += 1
-        else:
-            inventory[item] = 1
-    return inventory
 
 
 def add_items(inventory, items):
