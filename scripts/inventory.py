b14f13292a92a1263ccb7636ec18088cad225e32
Angelica <Angelica137@users.noreply.github.com>
2022-04-02 20:49:34 +0100

    add add_items(), green

--- scripts/inventory.py
+++ scripts/inventory.py
@@ -6,3 +6,12 @@ def create_inventory(items: list) -> dict:
         else:
             inventory[item] = 1
     return inventory
+
+
+def add_items(inventory, items):
+    for item in items:
+        if item in inventory:
+            inventory[item] += 1
+        else:
+            inventory[item] = 1
+    return inventory
